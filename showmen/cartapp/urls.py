00620db4e87c908b6d4443e5401a6ccd4762f669
adilyb <muhammedadilyb@gmail.com>
2024-03-03 22:55:08 +0530

    Add Product To Cart

--- showmen/cartapp/urls.py
+++ showmen/cartapp/urls.py
@@ -3,7 +3,7 @@ from . views import *
 
 urlpatterns = [
     path('', cart, name='cart'),
-    path('add/', add, name='add'),
+    path('add/', add, name='add_cart'),
     path('update/', update, name='update'),
     path('delete/', delete, name='delete'),
 
--- showmen/cartapp/views.py
+++ showmen/cartapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from mainapp.models import Product
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -7,8 +9,25 @@ def cart(request):
     return render(request, 'cart.html', {})
 
 def add(request):
-    pass
-
+    #get the cart
+    cart = Cart(request)
+    #test for post
+    if request.POST.get('action') == 'post':
+        
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+        
+       
+        #lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+        
+        #save to session
+        cart.add(product=product)
+        
+        #return response
+        response = JsonResponse({'Product Name': product.product_name})
+        return response
+        
 def update(request):
     pass
 
